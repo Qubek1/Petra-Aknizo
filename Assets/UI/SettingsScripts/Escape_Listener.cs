d4c6eb8cc072cefa04ab1b0c6e4e29bc49911dfc
Pharnem <61577936+Pharnem@users.noreply.github.com>
2020-04-03 01:44:33 +0200

    dodano działające i brzydkie UI"

---
+++ Assets/UI/SettingsScripts/Escape_Listener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escape_Listener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject pausemenu,settingsCanvas;
+    public Scene_Man_Script sms;
+    public bool paused;
+    void Start()
+    {
+        paused = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+            DoPause();
+        }
+        
+    }
+    public void SetPaused(bool x)
+    {
+        paused = x;
+        DoPause();
+    }
+    public void DoPause()
+    {
+        pausemenu.SetActive(paused);
+        sms.TimeStopper(!paused);
+        if (!paused)
+        {
+            settingsCanvas.SetActive(false);
+        }
+    }
+
+}
