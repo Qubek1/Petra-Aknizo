51c16de951fc948f98600965c41b27be000cc146
Pharnem <61577936+Pharnem@users.noreply.github.com>
2020-04-04 12:30:34 +0200

    Przygotownia do tworzenia poziomów

---
+++ Assets/UI/SettingsScripts/AuM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AuM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static AuM instance;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+            instance = this;
+        else 
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+    }
+}
---
+++ Assets/UI/SettingsScripts/Level_Chooser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level_Chooser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text lvl;
+    //Button btn;
+    public Scene_Man_Script sms;
+
+    void Start()
+    {
+        lvl = gameObject.GetComponent<Text>();
+        //btn = gameObject.GetComponent<Button>();
+        lvl.text = gameObject.name;
+        //btn.onClick.AddListener();
+    }
+
+    public void InvLoad()
+    {
+        sms.Load(gameObject.name);
+    }
+
+
+}
