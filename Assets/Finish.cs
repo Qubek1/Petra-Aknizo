e64f0c82b5740379e1c8695ab761b66a075f5211
Pharnem <61577936+Pharnem@users.noreply.github.com>
2020-04-05 15:44:26 +0200

    Dodano ściany znikające po wciśnięciu przycisku (należy podejść do przycisku gdy ma się ręce)

---
+++ Assets/Finish.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    /*string curr,nex;
+    int nex_num;
+    void Start()
+    {
+        curr = SceneManager.GetActiveScene().name;
+        int.TryParse(curr.Substring(2, 1), out nex_num);
+        nex_num += 1;
+        nex = curr.Substring(0, 2) + nex_num;
+    }*/
+    public string nex;
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag!="Player")
+        {
+            return;
+        }
+        /*if (SceneManager.GetSceneByName(nex).IsValid())
+        {*/
+            SceneManager.LoadScene(nex);
+        /*}
+        else
+        {
+            SceneManager.LoadScene("Start");
+        }*/
+    }
+}
---
+++ Assets/Open.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+    GameObject parent_tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent_tilemap = this.transform.parent.gameObject;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player" && col.gameObject.GetComponent<player_controller>().arms)
+        {
+            parent_tilemap.SetActive(false);
+        }
+    }
+}
