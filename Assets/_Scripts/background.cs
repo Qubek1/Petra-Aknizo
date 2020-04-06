fbb070cd95305f582b4a2bc3ecee5cef917ce7b7
qbek <44117365+Qba82@users.noreply.github.com>
2020-04-06 20:51:50 +0200

    tlo

---
+++ Assets/_Scripts/background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    public Transform[] Object;
+    public Transform Player;
+
+    void Update()
+    {
+        for(int i=0; i<Object.Length; i++)
+        {
+            Object[i].localPosition = new Vector3(-Player.position.x * Object[i].position.z / 10,
+                -Player.position.y * Object[i].position.z / 10,
+                Object[i].position.z);
+        }
+    }
+}
